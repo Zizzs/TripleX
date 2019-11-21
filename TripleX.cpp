32be47df2a37875890e44cae6f4517b3340a58e3
Alex Williams <Zizzs17@gmail.com>
2019-11-21 11:07:56 -0800

    C++/Unreal Naming Conventions and Refactoring.

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,21 +1,27 @@
+// Gives library functions for console printing. Similar to Using System in C#
 #include <iostream>
 
 int main()
 {
-    std::cout << "You are a secret agent breaking into a secure server room...";
+    // Standard Namespace, Cout function to post string to console.
+    // Print Initial Text
+    std::cout << "You are a secret agent breaking into a secure server room..." << std::endl;
+    std::cout << "Enter the correct code to continue..." << std::endl;
+
+    const int CodeA = 4;
+    const int CodeB = 3;
+    const int CodeC = 2;
+
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+
+    // Print CodeSum and CodeProduct to terminal
     std::cout << std::endl;
-    std::cout << "Enter the correct code to continue...";
+    std::cout << "+ There are 3 numbers in the code" << std::endl;
+    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
+    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
 
-    int a = 4;
-    int b = 3;
-    int c = 2;
-
-    int sum = a + b + c;
-    int product = a * b * c;
-
-    std::cout << std::endl;
-    std::cout << sum << std::endl;
-    std::cout << product << std::endl;
+    int PlayerGuess;
 
     return 0;
 }
